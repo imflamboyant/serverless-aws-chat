b2e65cb6557df312becf4be21e8360a96f0ceb7b
Marko Djakovic <clap021@gmail.com>
2024-12-14 10:56:38 +0100

    add message persistence in dynamodb via iot rule action

--- chat-iot-core/lib/chat-iot-core-stack.ts
+++ chat-iot-core/lib/chat-iot-core-stack.ts
@@ -2,8 +2,11 @@ import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {Code, Runtime} from 'aws-cdk-lib/aws-lambda';
 import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
-import {ServicePrincipal} from 'aws-cdk-lib/aws-iam';
+import {ManagedPolicy, Role, ServicePrincipal} from 'aws-cdk-lib/aws-iam';
 import {CfnAuthorizer} from 'aws-cdk-lib/aws-iot';
+import {AttributeType, BillingMode, ITable, Table} from 'aws-cdk-lib/aws-dynamodb';
+import {IotSql, TopicRule} from '@aws-cdk/aws-iot-alpha';
+import {DynamoDBv2PutItemAction} from '@aws-cdk/aws-iot-actions-alpha';
 
 export class ChatIotCoreStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -28,6 +31,22 @@ export class ChatIotCoreStack extends cdk.Stack {
             authorizerFunctionArn: authorizerLambda.functionArn,
             status: 'ACTIVE',
             signingDisabled: true,
+        });
+        // create a DynamoDB table for storing chat messages
+        const table: ITable = new Table(this, 'ChatMessagesTable', {
+            tableName: 'serverless-chat-iot-core-messages',
+            partitionKey: { name: 'channel', type: AttributeType.STRING },
+            sortKey: { name: 'timestamp', type: AttributeType.STRING },
+            billingMode: BillingMode.PAY_PER_REQUEST,
+        });
+        // create IoT Rule and Rule Action to send messages to the DynamoDB table
+        new TopicRule(this, 'SendMessagesToDynamoDB', {
+            topicRuleName: 'serverless_chat_iot_core_ddb_rule',
+            sql: IotSql.fromStringAsVer20160323('SELECT channel, parse_time("yyyy-MM-dd\'T\'HH:mm:ss.SSSz", timestamp()) AS timestamp, username, message FROM \'serverlesschat/channels/+\''),
+            enabled: true,
+            actions: [
+                new DynamoDBv2PutItemAction(table),
+            ],
         });
     }
 }
